01f30dd51eaf751ccc34ca7809109ff48dc07f39
Connor Dieck <connordieck@gmail.com>
2021-08-26 16:54:37 -0400

    added forms for registration, login, creating notes, and delete pathways

--- forms.py
+++ forms.py
@@ -1,7 +1,23 @@
 from flask_wtf import FlaskForm
 from flask_wtf.recaptcha import validators
-from wtforms import StringField, FloatField
+from wtforms import StringField, FloatField, PasswordField
 from wtforms.validators import Email, InputRequired
+from wtforms.fields.html5 import EmailField
 
 class RegisterForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+    email = EmailField("Email", validators=[InputRequired(), Email()])
+    first_name = StringField("First Name", validators=[InputRequired()])
+    last_name = StringField("Last Name", validators=[InputRequired()])
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+
+class NotesForm(FlaskForm):
+    text = StringField("Note Text", validators=[InputRequired()])
+    discussion_date = StringField("Discussion Date", validators=[InputRequired()])
+
+class DeleteForm(FlaskForm):
+    """Form used to send POST requests in delete pathways"""
