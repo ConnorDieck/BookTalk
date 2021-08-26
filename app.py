d804a7fc5905c8ccb7a6e9ac4c14fb75d3691c93
Connor Dieck <connordieck@gmail.com>
2021-08-26 11:25:38 -0400

    added imports

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, redirect, render_template
-from models import db, connect_db
+from models import db, connect_db, User, Book, Club, Membership, Reads, Notes
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///sqla_intro' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///booktalk' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 app.config['SQLALCHEMY_ECHO'] = True
 
